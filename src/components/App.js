aec839e0b3f2a56772720f3f56093ce85d29cafe
Rahul R <rahul.pld007@gmail.com>
2022-11-06 22:55:29 +0530

    commited

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [para, setPara] = useState("");
+  const [para, setPara] = useState();
   function clicked() {
     setPara(
       "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
     );
-    console.log("button clicked");
+    //console.log("button clicked");
   }
   return (
     <div id="main">
