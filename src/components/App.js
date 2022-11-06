e950cad4c133db5b79fbc71c72a0a3809d644348
Rahul R <rahul.pld007@gmail.com>
2022-11-06 20:06:59 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para, setPara] = useState("");
+  function clicked() {
+    setPara(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
+    console.log("button clicked");
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={clicked}>
+        Click me
+      </button>
+      <p id="para">{para}</p>
     </div>
   );
 }
-
 
 export default App;
