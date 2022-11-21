54f68c1e5dd5a1f8cb4a746a343d76841d8adc50
Rahul R <rahul.pld007@gmail.com>
2022-11-21 15:11:06 +0530

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,20 @@ import "./../styles/App.css";
 
 function App() {
   const [para, setPara] = useState();
-  const [click, setClick] = useState(false);
-
   function clicked() {
     setPara(
       "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
     );
-    setClick(true);
 
     //console.log("button clicked");
   }
 
-  const Para = () => {
-    return <p id="para">{para}</p>;
-  };
   return (
     <div id="main">
       <button id="click" onClick={clicked}>
         Click me
       </button>
-      {click && <Para />}
+      <p id="para">{para}</p>
     </div>
   );
 }
